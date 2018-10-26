71fa1c957bb18b961cd2658f521bc6b8316e4b13
Carlos Martinez <itscarlosisaac@gmail.com>
2018-10-26 12:35:53 -0400

    Added no break text helper class to copy to avoid widow word.

--- src/components/atoms/ContactBox.jsx
+++ src/components/atoms/ContactBox.jsx
@@ -4,7 +4,10 @@ const ContactBox = (props) => {
   return (
     <div className="app__contact__box">
       <div className="app__contact__box__info">
-        <h1>Interested on working with me?</h1>
+        <h1>
+          Interested on working
+          <span className="no-break-text"> with me?</span>
+        </h1>
         <h3>or just want to say hello.</h3>
       </div>
       <div className="app__contact__box__action">
