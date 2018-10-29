4a66fc5984ed066741bca9130003636e63a670c6
Carlos Martinez <itscarlosisaac@gmail.com>
2018-10-29 19:56:28 -0400

    Restored homepage react components.

---
+++ src/data/data.js
@@ -0,0 +1,31 @@
+export default () => (
+  {
+    companies: [
+      {
+        name: 'Sitata',
+        imageUrl: 'https://sitata.com/images/sitata-logo-horiz-white-e22fefa2453ef3fd95212b477e2b4591.png',
+        link: 'https://sitata.com/en/',
+      },
+    ],
+    projects: [
+      {
+        thumnailUrl: '',
+        title: '',
+        description: '',
+        link: '',
+      },
+    ],
+    portfolio: [
+      {
+        company: '',
+        type: '',
+        description: '',
+        thumbnail: '',
+        link: '',
+      },
+    ],
+    skills: [
+      { name: 'HTML5', years: 6 },
+    ],
+  }
+);
