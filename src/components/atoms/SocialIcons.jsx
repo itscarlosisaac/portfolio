c08ae550d8384d5d64a30b5fa17c224bdb271cb2
Carlos Martinez <itscarlosisaac@gmail.com>
2018-11-05 16:53:55 -0400

    Changed dribbled social with codesignal icon.

--- src/components/atoms/SocialIcons.jsx
+++ src/components/atoms/SocialIcons.jsx
@@ -6,7 +6,8 @@ const SocialIcons = (props) => {
   const social = [
     { name: 'linkedIn', image: `../../public/img/linkedin_${type}.svg`, link: 'https://www.linkedin.com/in/cisaacmb/' },
     { name: 'github', image: `../../public/img/github-alt_${type}.svg`, link: 'https://github.com/itscarlosisaac' },
-    { name: 'dribble', image: `../../public/img/dribbble_${type}.svg`, link: 'https://dribbble.com/itscarlosisaac' },
+    // { name: 'dribble', image: `../../public/img/dribbble_${type}.svg`, link: 'https://dribbble.com/itscarlosisaac' },
+    { name: 'codesignal', image: `../../public/img/codesignal_${type}.svg`, link: 'https://app.codesignal.com/profile/carlosisaac' },
   ];
   return (
     <div className="app__social">
